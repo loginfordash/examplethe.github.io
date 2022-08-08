dd14e4aefabe6123038ff516453ab4683a2021c2
loginfordash <69426293+loginfordash@users.noreply.github.com>
2022-08-08 05:53:55 +0500

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,4 @@
+var a = document.querySelector('a[href="https://www.dashline.uk/brandLogo"]');
+if (a) {
+  a.setAttribute('href', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_92x30dp.png')
+}
